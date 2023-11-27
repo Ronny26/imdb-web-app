af75ce1780f82b4253c2e32e22478c471ae50a45
Natasha Mundada <natashamundada@NatashaMundada.local>
2023-11-26 23:05:44 -0800

    sign in/out changes

--- src/components/users/authenticateUser.js
+++ src/components/users/authenticateUser.js
@@ -1,21 +1,42 @@
+import React, { createContext, useContext, useState } from "react";
 import * as client from "./client";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-function AuthenticateUser() {
-  const [credentials, setCredentials] = useState({ username: "", password: "" });
-  const navigate = useNavigate();
-  const signin = async () => {
-    await client.signin(credentials);
-    navigate("/kanbas/account");
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState();
+
+  const signin = async (credentials) => {
+    try{
+        const user = await client.signin(credentials);
+        setCurrentUser(user);
+        console.log(user)
+        return user;
+    } catch(error) {
+        throw error;
+    }
+    
   };
+
+  const signout = async () => {
+    await client.signout();
+    setCurrentUser(null);
+  };
+
+
   return (
-    <div style={{marginLeft:100}}>
-      <h1>Signin</h1>
-      <input value={credentials.username} onChange={(e) => setCredentials({...credentials, username: e.target.value})}/>
-      <br/>
-      <input value={credentials.password} onChange={(e) => setCredentials({...credentials, password: e.target.value})}/>
-      <button onClick={signin} className="btn btn-primary"> Signin </button>
-    </div>
+    <AuthContext.Provider value={{ currentUser, signin, signout, setCurrentUser }}>
+      {children}
+    </AuthContext.Provider>
   );
-}
-export default AuthenticateUser;
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
+export default useAuth;
