f563c4e5b1d91c9f2c1a423c6d0528a6da38a9b2
Ronny26 <priyanshushrivastava786@gmail.com>
2023-12-11 16:03:08 -0500

    popular

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import * as client from './client'
 import './Detail.css'
+import PopularMovies from '../../components/Movie/popular'
 
 const MovieInfoComponent = () => {
   const { movieId } = useParams()
@@ -25,33 +26,37 @@ const MovieInfoComponent = () => {
     return <div>Loading...</div>
   }
   return (
-    <div className='movie-detail'>
-      <div className='movie-detail-left'>
-        <h1 className='movie-title'>{movieInfo.originalTitleText.text}</h1>
-        <div className='movie-year-genre'>
-          <span className='movie-year'>{movieInfo.titleType.text}</span>
-          <span className='movie-year'>{movieInfo.releaseYear.year}</span>
-          <div className='movie-genres'>{}</div>
+    <div>
+      <div className='movie-detail'>
+        <div className='movie-detail-left'>
+          <h1 className='movie-title'>{movieInfo.originalTitleText.text}</h1>
+          <div className='movie-year-genre'>
+            <span className='movie-year'>{movieInfo.titleType.text}</span>
+            <span className='movie-year'>{movieInfo.releaseYear.year}</span>
+            <div className='movie-genres'>{}</div>
+          </div>
+          <div className='movie-rating'>{}</div>
+          <p className='movie-description'>{movieInfo.description}</p>
+          <div className='movie-tags'>{}</div>
+          <div className='movie-cast'>{}</div>
+          <div className='movie-actions'>
+            <button className='button-trailer'>Watch Trailer</button>
+          </div>
         </div>
-        <div className='movie-rating'>{}</div>
-        <p className='movie-description'>{movieInfo.description}</p>
-        <div className='movie-tags'>{}</div>
-        <div className='movie-cast'>{}</div>
-        <div className='movie-actions'>
-          <button className='button-trailer'>Watch Trailer</button>
+        <div className='movie-detail-right'>
+          <img
+            src={
+              movieInfo.primaryImage
+                ? movieInfo.primaryImage.url
+                : 'https://www.dotyeti.com/wp-content/uploads/2023/01/barbie.webp'
+            }
+            alt='Movie Poster'
+            className='movie-poster'
+          />
         </div>
       </div>
-      <div className='movie-detail-right'>
-        <img
-          src={
-            movieInfo.primaryImage
-              ? movieInfo.primaryImage.url
-              : 'https://www.dotyeti.com/wp-content/uploads/2023/01/barbie.webp'
-          }
-          alt='Movie Poster'
-          className='movie-poster'
-        />
-      </div>
+      <h2 className='title'>Most Popular Movies</h2>
+      <PopularMovies />
     </div>
   )
 }
