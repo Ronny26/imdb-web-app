3ad2b212a7fd1ae8e25e8954d476136a130abf69
Vamshika <lvamshika09@gmail.com>
2023-12-12 03:45:28 -0500

    details

--- src/pages/Details/client.js
+++ src/pages/Details/client.js
@@ -1,22 +1,28 @@
 import axios from 'axios'
 
-const BASE_API = 'https://moviesdatabase.p.rapidapi.com/titles'
-export const TITLES_API = `${BASE_API}`
+const DB_BASE_API = "http://localhost:4000";
+export const MOVIE_API = `${DB_BASE_API}/api/testMovies`;
+export const REVIEWS_API = `${DB_BASE_API}/api/testReviews`;
 
-const headers = {
-  headers: {
-    'X-RapidAPI-Key': 'cb8a9b638dmsh374f7c8f8dc4c8cp161ff6jsn9845b2679a4e',
-    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+export const getMovieById = async id => {
+  try {
+    const url = `${MOVIE_API}/${id}`
+    console.log(id)
+    const response = await axios.get(url)
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error(error)
   }
 }
 
-export const getTitles = async id => {
+export const getUserReviews = async (movieId) => {
   try {
-    const url = `${TITLES_API}/${id}`
-    console.log(id)
-    const response = await axios.get(url, headers)
-    console.log(response)
-    return response.data.results
+    const url = `${REVIEWS_API}/movie/${movieId}`
+    console.log(movieId)
+    const response = await axios.get(url)
+    console.log(response.data)
+    return response.data
   } catch (error) {
     console.error(error)
   }
--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,59 +1,179 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
-import * as client from './client'
-import './Detail.css'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import * as client from "./client";
+import "./Detail.css";
+import useAuth from "../../components/users/authenticateUser";
 
-const MovieInfoComponent = () => {
-  const { movieId } = useParams()
-  const [movieInfo, setMovieInfo] = useState('')
+const MovieDetails = () => {
+  const [userReviews, setUserReviews] = useState([]);
+  const [criticReviews, setCriticReviews] = useState([]);
+  const [showUserReviews, setShowUserReviews] = useState(false);
+  const [showCriticReviews, setShowCriticReviews] = useState(false);
+  const { movieId } = useParams();
+
+  const [movieData, setMovieData] = useState({
+    title: "The Color Purple",
+    actors: ["Alice Johnson", "David Thompson"],
+    description: "The Color Purple is a heartwarming drama...",
+    rating: 6.8,
+    primary_image: {
+      url: "https://m.media-amazon.com/images/M/MV5BYjBkNGE0NGYtYmU5Ny00NjRiLTk5MmYtMWU4NzYxMDE4YWY4XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+    },
+  });
 
   const getMovies = async () => {
     try {
-      const response = await client.getTitles(movieId)
-      setMovieInfo(response)
+      const response = await client.getMovieById(movieId);
+      console.log(response);
+      setMovieData(response);
     } catch (error) {
-      console.error('Error fetching movies:', error)
+      console.error("Error fetching movie:", error);
     }
-  }
+  };
+
+  const getUserReviews = async (movieId) => {
+    try {
+      const response = await client.getUserReviews(movieId);
+      console.log(response);
+      setUserReviews(response);
+    } catch (error) {
+      console.error("Error fetching movie:", error);
+    }
+  };
 
   useEffect(() => {
-    getMovies(movieId)
-  }, [])
+    getMovies(movieId);
+    getUserReviews(movieId);
+  }, []);
 
-  if (!movieInfo) {
-    return <div>Loading...</div>
+  const userReviewsData = [
+    { username: "User1", rating: 8, feedback: "Great movie!" },
+    { username: "User2", rating: 7, feedback: "Enjoyed it, but..." },
+  ];
+
+  const criticReviewsData = [
+    { criticName: "Critic1", rating: 9, feedback: "A masterpiece!" },
+    { criticName: "Critic2", rating: 8, feedback: "Solid performances..." },
+  ];
+
+  const handleShowUserReviews = () => {
+    setShowUserReviews(true);
+    setShowCriticReviews(false);
+  };
+
+  const handleShowCriticReviews = () => {
+    setShowCriticReviews(true);
+    setShowUserReviews(false);
+  };
+
+  if (!movieData) {
+    return <div>Loading...</div>;
   }
-  return (
-    <div className='movie-detail'>
-      <div className='movie-detail-left'>
-        <h1 className='movie-title'>{movieInfo.originalTitleText.text}</h1>
-        <div className='movie-year-genre'>
-          <span className='movie-year'>{movieInfo.titleType.text}</span>
-          <span className='movie-year'>{movieInfo.releaseYear.year}</span>
-          <div className='movie-genres'>{}</div>
-        </div>
-        <div className='movie-rating'>{}</div>
-        <p className='movie-description'>{movieInfo.description}</p>
-        <div className='movie-tags'>{}</div>
-        <div className='movie-cast'>{}</div>
-        <div className='movie-actions'>
-          <button className='button-trailer'>Watch Trailer</button>
-        </div>
-      </div>
-      <div className='movie-detail-right'>
-        <img
-          src={
-            movieInfo.primaryImage
-              ? movieInfo.primaryImage.url
-              : 'https://www.dotyeti.com/wp-content/uploads/2023/01/barbie.webp'
-          }
-          alt='Movie Poster'
-          className='movie-poster'
-        />
-      </div>
-    </div>
-  )
-}
+  // Uncomment and set up state based on your needs
+  // const [userRating, setUserRating] = useState(0);
+  // const [userFeedback, setUserFeedback] = useState('');
 
-export default MovieInfoComponent
+  // const handleUserSubmit = (e) => {
+  //   e.preventDefault();
+  //   const newUserReview = {
+  //     username: "New User",
+  //     rating: userRating,
+  //     feedback: userFeedback,
+  //   };
+  //   setUserReviews([...userReviews, newUserReview]);
+  //   // You may want to send this data to a server or update it in your state management
+  //   setUserRating(0);
+  //   setUserFeedback('');
+  // };
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-4">
+          <img
+            src={movieData.primary_image.url}
+            alt={movieData.title}
+            className="im-poster"
+          />
+        </div>
+        <div className="col-md-8">
+          <h1>{movieData.title}</h1>
+          <p>Actors: {movieData.actors.join(", ")}</p>
+          <p>Description: {movieData.description}</p>
+          <p>Rating: {movieData.rating}</p>
+          <button className="btn btn-primary" onClick={handleShowUserReviews}>
+            Show User Reviews
+          </button>
+          <button
+            className="btn btn-primary im-button"
+            onClick={handleShowCriticReviews}
+          >
+            Show Critic Reviews
+          </button>
+          <h3>Add Your Review</h3>
+          <div className="col-xs-3">
+            <label htmlFor="userRating">Rating (out of 10):</label>
+            <input
+              type="number"
+              className="form-control"
+              id="userRating"
+              min="0"
+              max="10"
+              // value={userRating}
+              // onChange={(e) => setUserRating(e.target.value)}
+            />
+          </div>
+          <div className="col-xs-6">
+            <label htmlFor="userFeedback">Feedback:</label>
+            <textarea
+              className="form-control"
+              id="userFeedback"
+              rows="3"
+              // value={userFeedback}
+              // onChange={(e) => setUserFeedback(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="mt-3">
+            {/* Uncomment the onSubmit event handler when ready to implement */}
+            <button type="submit" className="btn btn-primary">
+              Submit Review
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {showUserReviews && (
+        <div className="mt-4">
+          <h2>User Reviews</h2>
+          <ul className="list-group">
+            {userReviews.map((review, index) => (
+              <li key={index} className="list-group-item">
+                <p>{review.username}</p>
+                <p>Rating: {review.rating}</p>
+                <p>{review.comment}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {showCriticReviews && (
+        <div className="mt-4">
+          <h2>Critic Reviews</h2>
+          <ul className="list-group">
+            {criticReviewsData.map((review, index) => (
+              <li key={index} className="list-group-item">
+                <p>{review.criticName}</p>
+                <p>Rating: {review.rating}</p>
+                <p>{review.feedback}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MovieDetails;
