b94c43c2012b92ed98f948f1e4a9c9a667cae29a
Natasha Mundada <natashamundada@NatashaMundada.local>
2023-12-13 20:22:57 -0800

    blog link

--- src/pages/Details/client.js
+++ src/pages/Details/client.js
@@ -32,7 +32,7 @@ export const getMovieById = async (id) => {
       console.log(ratingsResponse)
       movie = {
         title: movieResponse.data.results.titleText.text ? movieResponse.data.results.title : "",
-        primary_image: movieResponse.data.results.primaryImage ? movieResponse.data.results.primaryImage.url : {url:"https://www.dotyeti.com/wp-content/uploads/2023/01/barbie.webp"},
+        primaryImage: movieResponse.data.results.primaryImage ? movieResponse.data.results.primaryImage.url : {url:"https://www.dotyeti.com/wp-content/uploads/2023/01/barbie.webp"},
         actors: actorsResponse.data.results ? actorsResponse.data.results : [],
         rating: ratingsResponse.data.results ? ratingsResponse.data.results.averageRating : "0",
       }
@@ -76,7 +76,7 @@ export const findUserById = async (userId) => {
 export const createReview = async (review) => {
   try {
     const response = await axios.post(`${REVIEWS_API}/review`, review);
-    console.log(response.data);
+    console.log("review", response.data);
     return response.data;
   } catch (error) {
     console.error("Error creating review:", error);
